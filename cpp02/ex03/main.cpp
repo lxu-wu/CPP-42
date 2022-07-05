b8dbd9e5ea130d20b090551ddb8b821c978ce535
Lei jie Xu wu <lxu-wu@fu-r5-p5.s19.be>
2022-07-05 13:29:57 +0200

    cxxflags

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -2,14 +2,27 @@
 
 int main( void ) 
 {
-	Point a(0, 0);
-	Point b(0, 100);
-	Point c(100, 0);
-	Point point(1, 1);
+	{
+		Point a(0, 0);
+		Point b(0, 100);
+		Point c(100, 0);
+		Point point(1, 1);
 
-	if (bsp(Point(a), Point(b), Point(c), Point(point)))
-		std::cout << "point is in the triangle a,b,c" << std::endl;
-	else
-		std::cout << "point is out the triangle a,b,c" << std::endl;
+		if (bsp(Point(a), Point(b), Point(c), Point(point)))
+			std::cout << "point is in the triangle a,b,c" << std::endl;
+		else
+			std::cout << "point is out the triangle a,b,c" << std::endl;
+	}
+	{
+		Point a(-50, 0);
+		Point b(0, 100);
+		Point c(50, 0);
+		Point point(0, -1);
+
+		if (bsp(Point(a), Point(b), Point(c), Point(point)))
+			std::cout << "point is in the triangle a,b,c" << std::endl;
+		else
+			std::cout << "point is out the triangle a,b,c" << std::endl;
+	}
 	return 0;
 }
